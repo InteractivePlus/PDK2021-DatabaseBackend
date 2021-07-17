d118858903e68d887833c1b7391988b2a085a435
Windy Feng <1745500559@qq.com>
2021-07-17 14:40:31 +0800

    Some work on VeriCodeFactory

--- src/VeriCodeFactoryMySQL.ts
+++ src/VeriCodeFactoryMySQL.ts
@@ -1,23 +1,30 @@
 import mysql, {Connection} from 'mysql2';
 import {VerificationCodeEntityFactory, VerificationCodeCreateEntity} from "@interactiveplus/pdk2021-backendcore/dist/AbstractFactoryTypes/Communication/VerificationCode/VerificationCodeEntityFactory";
 import {VerificationCodeEntity} from "@interactiveplus/pdk2021-common/dist/AbstractDataTypes/Communication/VerificationCode/VerificationCodeEntity"
-import { BackendCommunicationSystemSetting } from '../../PDK2021-BackendCore/dist/AbstractDataTypes/SystemSetting/BackendCommunicationSystemSetting';
+import { BackendCommunicationSystemSetting } from '@interactiveplus/pdk2021-backendcore/dist/AbstractDataTypes/SystemSetting/BackendCommunicationSystemSetting';
 
-class vericodeMysql implements VerificationCodeEntityFactory<any>{
-    constructor(public mysqlConnection:Connection) {}
+interface VericodeFactoryMySQLVerifyInfo{
+    code: string,
+    isShortCode: boolean
+}
 
-    // getCommunicationSystemSetting(): BackendCommunicationSystemSetting;
-    // createVerificationCode<ParamType>(createInfo: VerificationCodeCreateEntity<ParamType>): VerificationCodeEntity<ParamType>;
-    // revokeCreatedVerificationCode<ParamType>(createdVeriCodeEntity: VerificationCodeEntity<ParamType>): void;
-    // verifyVerificationCode(verifyInfo: VerifyCodeInfo): boolean;
-    // verifyAndUseVerificationCode(verifyInfo: VerifyCodeInfo): boolean;
-    // /**
-    //  * Check if verifyInfo is in correct format
-    //  * @param verifyInfo struct passed by client
-    //  * @returns {VerifyCodeInfo} Parsed CodeInfo
-    //  * @throws {PDKRequestParamFormatError}
-    //  */
-    // checkVerifyInfoValid(verifyInfo: any): VerifyCodeInfo;
+class VericodeFactoryMySQL implements VerificationCodeEntityFactory<VericodeFactoryMySQLVerifyInfo>{
+    constructor(public mysqlConnection:Connection, private communicationSystemSetting : BackendCommunicationSystemSetting) {}
+
+    getCommunicationSystemSetting(): BackendCommunicationSystemSetting{
+        return this.communicationSystemSetting;
+    }
+
+    createVerificationCode<ParamType>(createInfo: VerificationCodeCreateEntity<ParamType>): Promise<VerificationCodeEntity<ParamType>>{
+        
+    };
+    revokeCreatedVerificationCode<ParamType>(createdVeriCodeEntity: VerificationCodeEntity<ParamType>): Promise<void>;
+    verifyVerificationCode(verifyInfo: VericodeFactoryMySQLVerifyInfo): boolean;
+    verifyAndUseVerificationCode(verifyInfo: VericodeFactoryMySQLVerifyInfo): boolean;
+    
+    checkVerifyInfoValid(verifyInfo: any): VericodeFactoryMySQLVerifyInfo{
+        
+    }
     // getVerificationCode?(veriCodeID: VeriCodeEntityID): VerificationCodeEntity<unknown> | undefined;
     // updateVerificationCode?<ParamType>(veriCodeID: VeriCodeEntityID, veriCode: VerificationCodeEntity<ParamType>, oldVeriCode?: VerificationCodeEntity<ParamType>): void;
     // deleteVerificationCode?(veriCodeID: VeriCodeEntityID): void;
@@ -26,24 +33,24 @@ class vericodeMysql implements VerificationCodeEntityFactory<any>{
     // searchVerificationCode?(veriCodeID?: VeriCodeEntityID, isShortID?: boolean, relatedUser?: UserEntityUID, relatedAPP?: APPUID, relatedMaskID?: MaskUID, relatedAPPClientID?: APPClientID, relatedAPPOAuthToken?: OAuthAccessToken, triggerClientIP?: string, issueUTCTimeMin?: number, issueUTCTimeMax?: number, expireUTCTimeMin?: number, expireUTCTimeMax?: number, useScope?: string | number, used?: boolean, sentMethod?: CommunicationMethodWithNone, numLimit?: number, startPosition?: number): SearchResult<VerificationCodeEntity<unknown>>;
     // clearVerificationCode?(veriCodeID?: VeriCodeEntityID, isShortID?: boolean, relatedUser?: UserEntityUID, relatedAPP?: APPUID, relatedMaskID?: MaskUID, relatedAPPClientID?: APPClientID, relatedAPPOAuthToken?: OAuthAccessToken, triggerClientIP?: string, issueUTCTimeMin?: number, issueUTCTimeMax?: number, expireUTCTimeMin?: number, expireUTCTimeMax?: number, useScope?: string | number, used?: boolean, sentMethod?: CommunicationMethodWithNone, numLimit?: number, startPosition?: number): SearchResult<VerificationCodeEntity<unknown>>;
 
-    creatTable() {
+    install() : Promise<void> {
         //SHA1
         let tableCommand = `CREATE TABLE verifyCode 
                             (   
-                                'veriCodeID' TEXT,
-                                'isShortID' BOOLEAN NOT NULL DEFAULT 0,
+                                'veriCodeID' VARCHAR(100),
+                                'isShortID' TINYINT(1) NOT NULL,
                                 'relatedUser' VARCHAR(100),
                                 'relatedAPP' VARCHAR(100),
                                 'relatedMaskID' VARCHAR(100),
                                 'relatedAPPClientID' VARCHAR(100),
                                 'relatedOAuthToken' VARCHAR(100),
-                                'param' JSON NOT NULL,
-                                'triggerClientIP' TEXT,
+                                'param' JSON,
+                                'triggerClientIP' VARCHAR(45),
                                 'issueUTCTime' INT UNSINGED,
                                 'expireUTCTime' INT UNSINGED,
                                 'useScope' VARCHAR(100),
-                                'used' BOOLEAN NOT NULL DEFAULT 0,
-                                'sentMethod' JSON NOT NULL,
+                                'used' TINYINT(1) NOT NULL,
+                                'sentMethod' TINYINT NOT NULL,
                                 PRIMATY KEY (veriCodeID)
                             );`;
         return new Promise((resolve, reject) => {
